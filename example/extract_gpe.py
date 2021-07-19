d224daf985f8a4553f5a2320a929036ab81f850f
Ian Ross <iross@cs.wisc.edu>
2021-07-19 14:34:32 -0500

    Update extract_gpe.py

--- example/extract_gpe.py
+++ example/extract_gpe.py
@@ -3,7 +3,6 @@ import spacy
 from os import path
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
 
 for doc in glob.glob("/input/*.txt"):
     with open(path.join("/output/", path.basename(doc).replace(".txt", "_gpe.txt")), "w") as fout:
